78d22d585dea61e6665a1fa4dcf57073ec12e246
Sleepy Rae <67649181+sleepyexe@users.noreply.github.com>
2024-02-17 04:29:08 +0700

    Revert "Initial Commit"
    This reverts commit 94869e2c7f7f74ca3e8aa623672cafc13665105d.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,9 +8,4 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  }
 });
